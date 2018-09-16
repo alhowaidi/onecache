eb1b573261cc76ad9c6b6b77a1c89690b8871586
Deepak Nadig <deepnadig@gmail.com>
2018-09-15 22:19:10 -0500

    Added NdnInfo.

---
+++ src/main/java/org/unl/cse/netgroup/NdnInfo.java
@@ -0,0 +1,47 @@
+package org.unl.cse.netgroup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class NdnInfo {
+
+    private String name;
+    private String interestFileResource;
+    private String interestSrc;
+    private String interestDst;
+
+    private static Logger log = LoggerFactory.getLogger(NdnInfo.class);
+
+    public NdnInfo(String name, String interestFileResource, String interestSrc, String interestDst) {
+        this.name = name;
+        this.interestFileResource = interestFileResource;
+        this.interestSrc = interestSrc;
+        this.interestDst = interestDst;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInterestFileResource() {
+        return interestFileResource;
+    }
+
+    public String getInterestSrc() {
+        return interestSrc;
+    }
+
+    public String getInterestDst() {
+        return interestDst;
+    }
+
+    public void logInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name:" ).append(this.name).append(", ")
+                .append("Interest File: ").append(this.interestFileResource).append(", ")
+                .append("Interest Src: ").append(this.interestSrc).append(", ")
+                .append("Interest Dst: ").append(this.interestDst);
+        log.info(sb.toString());
+    }
+}
